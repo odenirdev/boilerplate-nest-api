3d1efc25ac69a691cfe230298a4401e609967e77
Odenir Gomes <odenirdev@gmail.com>
2025-02-15 18:36:15 -0300

    wip

--- test/mocks/response.ts
+++ test/mocks/response.ts
@@ -1,8 +1,4 @@
 export const mockResponse = {
   status: jest.fn().mockReturnThis(),
   json: jest.fn().mockReturnThis(),
-  sendStatus: jest.fn().mockReturnThis(),
-  links: jest.fn().mockReturnThis(),
-  send: jest.fn().mockReturnThis(),
-  jsonp: jest.fn().mockReturnThis(),
 } as unknown as Response;
